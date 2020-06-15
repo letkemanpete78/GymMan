a0938325b9cdb5066cfeb9990b0ededc5e768755
peteletkeman78 <letkemanpete@gmail.com>
2020-06-15 16:23:43 -0400

    wire updates

--- wire.go
+++ wire.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"exercise"
-
 	"github.com/google/wire"
 	"github.com/jinzhu/gorm"
+	"github.com/letkemanpete78/gymman/exercise"
 )
 
-func initAPI(db *gorm.DB) exercise.API {
+func InitAPI(db *gorm.DB) exercise.API {
 	wire.Build(exercise.ProvideExerciseRepostiory, exercise.ProvideService, exercise.ProvideAPI)
 
 	return exercise.API{}
